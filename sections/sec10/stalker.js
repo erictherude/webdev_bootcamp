9bd0dadd2606f4094dc65ae483367e1b1782bc2d
Eric Kammerud <ekammerud@gmail.com>
2020-05-22 23:53:13 -0700

    Updated sec10/stalker.js with userFullName variable.

--- sections/sec10/stalker.js
+++ sections/sec10/stalker.js
@@ -2,5 +2,7 @@ let userFirstName = prompt("What is your first name?");
 let userLastName = prompt("What is your last name?");
 let userAge = prompt("How many years old are you?");
 
-console.log("Your full name is " + userFirstName + " " + userLastName + ".");
+let userFullName = userFirstName + " " + userLastName;
+
+console.log("Your full name is " + userFullName + ".");
 console.log("You are " + userAge + " years old.")
