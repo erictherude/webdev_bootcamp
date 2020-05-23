cb1bd61d489a5d31fba07ea5f95c347a3393fb4a
Eric Kammerud <ekammerud@gmail.com>
2020-05-22 23:46:58 -0700

    Adding stalker.html and stalker.js to sec10 for Lesson 121 - JS Stalker Exercise.

---
+++ sections/sec10/stalker.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("How many years old are you?");
+
+console.log("Your full name is " + userFirstName + " " + userLastName + ".");
+console.log("You are " + userAge + " years old.")
