308feb147dc8b59c3d545cf856cce7f028f422f1
Eric Kammerud <ekammerud@gmail.com>
2020-05-22 23:49:35 -0700

    Updated sec10/stalker.js to use let in place of var.

--- sections/sec10/stalker.js
+++ sections/sec10/stalker.js
@@ -1,6 +1,6 @@
-var userFirstName = prompt("What is your first name?");
-var userLastName = prompt("What is your last name?");
-var userAge = prompt("How many years old are you?");
+let userFirstName = prompt("What is your first name?");
+let userLastName = prompt("What is your last name?");
+let userAge = prompt("How many years old are you?");
 
 console.log("Your full name is " + userFirstName + " " + userLastName + ".");
 console.log("You are " + userAge + " years old.")
