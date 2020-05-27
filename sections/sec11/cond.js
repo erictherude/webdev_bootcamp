c267c5c2fe2ef1ce88b9c5314edb1ad0d5abb1dc
Eric Kammerud <ekammerud@gmail.com>
2020-05-26 21:50:53 -0700

    Updating cond.js for Lesson 130 - Conditionals Solution.

--- sections/sec11/cond.js
+++ sections/sec11/cond.js
@@ -1,14 +1,14 @@
-let age = prompt("Hom many years old are you?");
+let age = Number(prompt("Hom many years old are you?"));
 
 console.log(age);
 
 if(age < 0){
 	console.log("ERROR! That isn't a real age.");
 }
-else if(age == 21){
+else if(age === 21){
 	console.log("You are " + age + " years old. Happy 21st birthday!");
 }
-else if(age % 2 === 1){
+else if(age % 2 !== 0){
 	console.log("You are " + age + " years old. Your age is odd!");
 }
 else if(Math.sqrt(age) % 1 === 0){
