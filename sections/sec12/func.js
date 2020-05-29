628b97a86f93ee5cbd3015e2a7b407cbc4d61f94
Eric Kammerud <ekammerud@gmail.com>
2020-05-29 00:18:25 -0700

    Added fun.html and func.js for Lesson 149 - Functions Problem Set. Updated js-notes-s12.txt with additional info on return.

---
+++ sections/sec12/func.js
@@ -0,0 +1,44 @@
+// isEven() - takes a single numeric argument and returns true 
+// for even numbers and false for others.
+
+function isEven(num) {
+	if(num % 2 === 0){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// isEven(4);		// true
+// isEven(21);		// false
+// isEven(68);		// true
+// isEven(333);	// false
+
+// factorial() - takes a single numeric argument and returns 
+// it's factorial.
+
+function factorial(num) {
+	let numFac = 1;
+
+	for(i = num; i > 0; i--) {
+		numFac = numFac * i;
+	}
+
+	return numFac;
+}
+
+// factorial(5);	// 120
+// factorial(2);	// 2
+// factorial(10);	// 3628800
+// factorial(0);	// 1
+
+// kebabToSnake() - takes a single kebab-cased string argument
+// and returns the snake_cased version.
+
+function kebabToSnake(str) {
+	return str.replace(/-/g,"_");
+}
+
+// kebabToSnake("hello-world");
+// kebabToSnake("dogs-are-awesome");
+// kebabToSnake("blah");
