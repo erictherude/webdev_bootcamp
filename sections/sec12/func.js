f78951d2357d26566be68c358f8322c809c66aca
Eric Kammerud <ekammerud@gmail.com>
2020-05-29 00:36:17 -0700

    Updating func.js for Lesson 150 - Function Problem Set Solutions.

--- sections/sec12/func.js
+++ sections/sec12/func.js
@@ -1,18 +1,23 @@
 // isEven() - takes a single numeric argument and returns true 
 // for even numbers and false for others.
 
-function isEven(num) {
-	if(num % 2 === 0){
+/*function isEven(num) {
+	if(num % 2 === 0) {
 		return true;
 	} else {
 		return false;
 	}
+}*/
+
+// refactor from solution vid
+function isEven(num) {
+	return num % 2 === 0;
 }
 
 // isEven(4);		// true
 // isEven(21);		// false
 // isEven(68);		// true
-// isEven(333);	// false
+// isEven(333);		// false
 
 // factorial() - takes a single numeric argument and returns 
 // it's factorial.
@@ -20,8 +25,8 @@ function isEven(num) {
 function factorial(num) {
 	let numFac = 1;
 
-	for(i = num; i > 0; i--) {
-		numFac = numFac * i;
+	for(i = num; i > 1; i--) {
+		numFac *= i;
 	}
 
 	return numFac;
