c563603d065cb55f82e651bc58231bbc34150bb9
Eric Kammerud <ekammerud@gmail.com>
2020-06-01 21:12:55 -0700

    Updated todo.html and todo.js for Lesson 163 - Todo List Code Along, 2.

--- sections/sec13/todo.js
+++ sections/sec13/todo.js
@@ -3,19 +3,40 @@ let todos = ["Buy New Turtle"];
 let input = prompt("What would you like to do?")
 
 while(input !== "quit") {
-
 	if(input === "list") {
-		console.log(todos);
+		listTodos();
 	} else if(input === "new") {
-		// ask for new todo
-		let newTodo = prompt("What would you like to add?");
-
-		// add new todo to array
-		todos.push(newTodo);
+		newTodo();
+	} else if(input === "delete"){
+		deleteTodo();
 	}
 
 	input = prompt("What would you like to do?")
-
 }
 
 console.log("Goodbye!");
+
+function listTodos() {
+	todos.forEach(function(todo, i){
+		console.log("********************************");
+		console.log(i + ": " + todo);
+		console.log("********************************");
+	})
+}
+
+function newTodo() {
+	// ask for new todo
+	let newTodo = prompt("What would you like to add?");
+
+		// add new todo to array
+		todos.push(newTodo);
+		console.log("Todo added.");
+}
+
+function deleteTodo() {
+		// ask for index of todo to be deleted
+		let index = prompt("Enter the index of the Todo to delete.")
+		// delete that todo
+		todos.splice(index, 1);
+		console.log("Todo deleted.");
+}
