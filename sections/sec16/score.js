5790d95da1dd2375544039692592050b41ff3a97
Eric Kammerud <ekammerud@gmail.com>
2020-06-17 00:25:19 -0700

    Updated score.html and score.js and added score.css for Lesson 192 - Score Keeper, Pt. 2.

--- sections/sec16/score.js
+++ sections/sec16/score.js
@@ -6,18 +6,22 @@ let p2Display = document.querySelector("#p2Display");
 
 let resetButton = document.querySelector("#reset");
 
+let numInput = document.querySelector("#goal");
+let winningScoreDisplay = document.querySelector("#winningScore") 
+
 let p1Score = 0;
 let p2Score = 0;
 
 let gameOver = false;
-let winnigScore = 5;
+let winningScore = 5;
 
 p1Button.addEventListener("click", function() {
 	if (!gameOver) {
 		p1Score++;
 		p1Display.textContent = p1Score;
 
-		if (p1Score === winnigScore) {
+		if (p1Score === winningScore) {
+			p1Display.classList.add("winner");
 			gameOver = true;
 		}
 	}
@@ -28,18 +32,32 @@ p2Button.addEventListener("click", function() {
 		p2Score++;
 		p2Display.textContent = p2Score;
 
-		if (p2Score === winnigScore) {
+		if (p2Score === winningScore) {
+			p2Display.classList.add("winner");
 			gameOver = true;
 		}
 	}
 });
 
-
-resetButton.addEventListener("click", function() {
+function resetGame() {
 	p1Score = 0;
 	p1Display.textContent = p1Score;
+	p1Display.classList.remove("winner");
+
 	p2Score = 0;
 	p2Display.textContent = p2Score;
+	p2Display.classList.remove("winner");
 
 	gameOver = false;
+};
+
+resetButton.addEventListener("click", function() {
+	resetGame();
+});
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	console.log(Number(this.value));
+	resetGame();
 });
