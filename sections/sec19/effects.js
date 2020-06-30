34b00fa10e3f91cce18a069061f9f6b1c059ba3c
Eric Kammerud <ekammerud@gmail.com>
2020-06-30 16:11:34 -0700

    Updated Sec19 JS Notes file and added jQueryEffects.html and effects.js for Lesson 220 - jQuery Effects.

---
+++ sections/sec19/effects.js
@@ -0,0 +1,26 @@
+// $("button").on("click", function(){
+// 	$("div").fadeOut(1000, function(){
+// 		$(this).remove();
+// 	});	
+// })
+
+// $("button").on("click", function(){
+// 	$("div").fadeIn();
+// 	});	
+
+// $("button").on("click", function(){
+// 	$("div").fadeToggle();
+// 	});	
+
+// $("button").on("click", function(){
+// 	$("div").slideDown();
+// 	});	
+
+// $("button").on("click", function(){
+// 	$("div").slideUp();
+// 	});	
+
+
+$("button").on("click", function(){
+	$("div").slideToggle();
+	});	
