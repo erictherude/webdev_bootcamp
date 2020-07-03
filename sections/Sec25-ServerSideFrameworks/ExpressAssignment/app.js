1ed8c26742c904ad5dd02d23629dbeddee3dcab8
erictherude <ekammerud@gmail.com>
2020-07-03 08:50:48 +0000

    Updated ExpressAssignment/app.js following Lesson 273 - Express Exercise Solution.

--- sections/Sec25-ServerSideFrameworks/ExpressAssignment/app.js
+++ sections/Sec25-ServerSideFrameworks/ExpressAssignment/app.js
@@ -1,8 +1,8 @@
 let express =	require("express");
-let app = express();
+let app = 		express();
 
 // ************
-// Routes
+// ROUTES
 // ************
 
 // Visiing */* should print *Hi there, welcome to my assignment!*
@@ -12,22 +12,38 @@ app.get("/", function(req, res){
 
 // One route */speak* that takes an animal and prints animal and it's sound for at least five animals
 app.get("/speak/:animal", function(req, res){
-	let animal = req.params.animal;
+	let sounds = {
+		pig:	"Oink.",
+		cow:	"Moo.",
+		dog:	"WOOF!",
+		cat:	"MEOW!!!",
+		horse:	"Neigh."
+	}
 	let sound = "";
 	
-	if(animal === "pig"){
-		sound = "Oink.";
-	} else if(animal === "cow") {
-		sound = "Moo.";
-	} else if(animal === "dog") {
-		sound = "WOOF!";
-	} else if(animal === "cat") {
-		sound = "MEOW!!!";
-	} else if(animal === "horse") {
-		sound = "Neigh.";
+	let animal = req.params.animal.toLowerCase();
+	if(sounds[animal]){
+		sound = sounds[animal];
 	} else {
-		sound = "Huh?"
-	}
+		sound = "Huh?";
+	};
+	
+	// // MY ORIGINAL SOLUTION
+	// let animal = req.params.animal
+	// let sound = "";
+	// if(animal === "pig"){
+	// 	sound = "Oink.";
+	// } else if(animal === "cow") {
+	// 	sound = "Moo.";
+	// } else if(animal === "dog") {
+	// 	sound = "WOOF!";
+	// } else if(animal === "cat") {
+	// 	sound = "MEOW!!!";
+	// } else if(animal === "horse") {
+	// 	sound = "Neigh.";
+	// } else {
+	// 	sound = "Huh?"
+	// };
 	
 	res.send("The " + animal + " says, \"" + sound + "\"");
 });
@@ -35,7 +51,7 @@ app.get("/speak/:animal", function(req, res){
 // One route */repeat* that takes a word and a number and prints the word number times
 app.get("/repeat/:str/:num", function(req, res){
 	let str = req.params.str;
-	let num = req.params.num;
+	let num = Number(req.params.num);
 	let response = "";
 	
 	for(i = 0; i < num; i++) {
@@ -47,11 +63,13 @@ app.get("/repeat/:str/:num", function(req, res){
 // All other routes should print *Sorry, page not found...What are you even doing?*
 app.get("*", function(req, res){
 	res.send("Sorry, page not found...What are you even doing?");
-})
+});
 
 
+// ************
+// LISTENER
+// ************
 
-// Listen for requests
 app.listen(3000, function() { 
   console.log('Server listening on port 3000'); 
 });
